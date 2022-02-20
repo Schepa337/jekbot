f64930f1815b0e8b972df7e91dc22128d3887900
Екатерина Ощепкова <77344404+Schepa337@users.noreply.github.com>
2022-02-20 14:03:22 +0500

    Add files via upload

---
+++ index_eng.php
@@ -0,0 +1,563 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Automated Trading with Jek_bot</title>
+	<link rel="stylesheet" href="fonts/stylesheet.css">
+	<link rel="stylesheet" href="main.css">
+	<link href="js/owl/assets/owl.carousel.min.css" rel="stylesheet">
+	<link href="js/owl/assets/owl.theme.default.min.css" rel="stylesheet">
+	<link href="js/fancybox/jquery.fancybox.min.css" rel="stylesheet">
+	<meta property="og:description" content="Automated Trading with Jek_bot" />
+	<meta name="description" content="Automated Trading with Jek_bot" />
+	<meta name="mrc__share_title" content="Jek_bot" />
+	<meta name="mrc__share_description" content="Automated Trading with Jek_bot" />
+	<link rel="image_src" href="https://" />
+	<meta property="og:title" content="Jek_bot" />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="Jekbot.ru" />
+	<meta property="og:site_name" content="Jek_bot" />
+	<meta property="og:locale" content="ru" />
+	<meta property="og:image" content="images/logo.png" />
+	<meta property="og:image:alt" content="Jek_bot" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/manifest.json">
+	<meta name="msapplication-TileColor" content="#ffffff">
+	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+	<meta name="theme-color" content="#ffffff">
+</head>         
+<body>
+	<header>
+		<div class="container hide-desktop">
+			<div class="row">
+				<img src="images/logo.png" alt="Jek_bot" class="logo">
+				<ul class="main-menu h6">
+					<li><a href="#dohodnost">Profitability</a></li>
+					<li><a href="#ATC">What is ATC</a></li>
+					<li><a href="#cnachat">Start earning</a></li>
+					<li><a href="#kontacti">Contact us</a></li>
+				</ul>
+				<a href="https://t.me/joinchat/AzR3g0pKUcXt-dIUT5rMgQ" target="_blank" class="btn" id="open_chat">Open chat</a>
+				<div class="flags-wrap">
+					<a class="flag flag_eng active" href="/index_eng.php">
+						<img src="images/flag.png">
+					</a>	
+					<a class="flag flag_ru" href="/index.php">
+						<img src="images/flag_2.png">
+					</a>	
+				</div>
+			</div>
+		</div>
+
+		<div class="container hide-mobile">
+			<div class="top-bar">
+				<div class="mobile-menu-wrap">
+					<p class="desc">Menu</p>
+				</div>
+				<a href="https://t.me/joinchat/AzR3g0pKUcXt-dIUT5rMgQ" target="_blank" class="btn btn-large" id="open_chat_mob">Open chat</a>
+				<div class="flags-wrap flags-wrap_mob">
+					<a class="flag flag_eng active" href="/index_eng.php">
+						<img src="images/flag.png">
+					</a>	
+					<a class="flag flag_ru" href="/index.php">
+						<img src="images/flag_2.png">
+					</a>	
+				</div>
+			</div>
+			<div class="menu-bar">
+				<ul class="main-menu menu-mob h6">
+					<li><a href="#dohodnost">Profitability</a></li>
+					<li><a href="#ATC">What is ATC</a></li>
+					<li><a href="#cnachat">Start earning</a></li>
+					<li><a href="#kontacti">Contact us</a></li>
+				</ul>
+			</div>
+		</div>
+
+	</header>
+
+	<section id="dohodnost" class="dohodnost">
+		<div class="numbers">
+			<ul>
+				<li class="active">01</li>
+				<li>02</li>
+				<li>03</li>
+				<li>04</li>
+				<li>05</li>
+				<li>06</li>
+				<li>07</li>
+				<li>08</li>
+			</ul>
+		</div>
+		<div class="container-fluid">
+
+			<div class="container">
+
+				<div class="columns">
+					<div class="col-left">
+						<div class="logo-mob">
+							<img src="images_mob/logo.png" alt="logo">
+						</div>
+						<div class="h1 title">Automated Trading with Jek_bot</div>
+						<div class="h4 desc">
+							JEK_BOT brings from <strong>60%</strong> per annum for the past <strong>3 years</strong>
+						</div>
+
+						<div class="buttons">						
+							<a href="https://t.me/joinchat/AzR3g0pKUcXt-dIUT5rMgQ" target="_blank" class="btn btn_tochat">Go to investor chat</a>
+							<div class="btn btn_outline">+ promocode caiman -15%</div>
+						</div>
+
+					</div>
+					<div class="col-right">
+						<img src="images/block_1_img.png" alt="Automated Trading">
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+	</section>
+	<section id="ATC" class="ATC">
+		<div class="numbers">
+			<ul>
+				<li>01</li>
+				<li class="active">02</li>
+				<li>03</li>
+				<li>04</li>
+				<li>05</li>
+				<li>06</li>
+				<li>07</li>
+				<li>08</li>
+			</ul>
+		</div>
+		<div class="container-fluid">
+			<div class="container">
+				<div class="h1 title">ATC Jek_bot Profitability</div>
+				<div class="images-slider owl-carousel owl-theme">
+				    <a data-fancybox="gallery" href="images/screen_01.jpg" class="item">
+				    	<img class="screen" src="images/screen_01.jpg">
+				    </a>
+				    <a data-fancybox="gallery" href="images/screen_02.jpg" class="item">
+				    	<img class="screen" src="images/screen_02.jpg">
+				    </a>
+				    <a data-fancybox="gallery" href="images/screen_03.jpg" class="item">
+				    	<img class="screen" src="images/screen_03.jpg">
+				    </a>
+				    <a data-fancybox="gallery" href="images/screen_04.jpg" class="item">
+				    	<img class="screen" src="images/screen_04.jpg">
+				    </a>
+				    <a data-fancybox="gallery" href="images/screen_05.jpg" class="item">
+				    	<img class="screen" src="images/screen_05.jpg">
+				    </a>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section id="user" class="user">
+		<div class="numbers">
+			<ul>
+				<li>01</li>
+				<li>02</li>
+				<li class="active">03</li>
+				<li>04</li>
+				<li>05</li>
+				<li>06</li>
+				<li>07</li>
+				<li>08</li>
+			</ul>
+		</div>
+		<div class="container-fluid">
+			<div class="container">
+				<div class="h1 title">Number of users</div>
+				<div class="h4 desc">Number of users from <br>2017 to 2020</div>
+				<div class="img-wrap img-wrap_desk">
+					<img class="user-img" src="images/img_1.png">
+				</div>
+				<div class="img-wrap img-wrap_mob">
+					<img class="user-img" src="images_mob/graphics.png">
+				</div>
+				<div class="list">
+					2017 - 1 user<br>
+					2018 - 47 users<br>
+					2019 - 512 users<br>
+					2020 - 547 users
+				</div>			
+			</div>
+		</section>
+		<section id="bot" class="bot">
+			<div class="numbers">
+				<ul>
+					<li>01</li>
+					<li>02</li>
+					<li>03</li>
+					<li class="active">04</li>
+					<li>05</li>
+					<li>06</li>
+					<li>07</li>
+					<li>08</li>
+				</ul>
+			</div>
+			<div class="container-fluid">
+				<div class="container">
+					<div class="h1 title">What is<br>АТС JEK_BOT?</div>
+					<div class="wrap wrap-text">
+						<div class="wrap-p">
+							<p> This is a program created by professional traders and programmers, it combines all the most modern strategies and techniques of market analysis.</p>
+							<p> The main task of any adviser is to simplify the decision-making process, up to its full automation.</p>
+							<p>That is, a multicurrency adviser is a program that is capable of</p>
+						</div>
+						<div class="wrap-p">
+							<p>automatically open and close transactions in the international multicurrency market.</p>
+							<p>Such an adviser connects to the trading terminal and makes transactions for you, automatically, according to the settings.</p>
+							<p>You can simply watch it work and make a profit.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="info" class="info">
+			<div class="numbers">
+				<ul>
+					<li>01</li>
+					<li>02</li>
+					<li>03</li>
+					<li>04</li>
+					<li class="active">05</li>
+					<li>06</li>
+					<li>07</li>
+					<li>08</li>
+				</ul>
+			</div>
+			<div class="container-fluid">
+				<div class="container">
+					<div class="stolbiki-wrap">
+						<div class="stolbik stolbik_1">
+							<div class="h1 title">Information</div>
+							<div class="h4 desc">
+								What is the international currency market? And how can absolutely  <br> anyone earn money in it...
+							</div>
+							<a href="https://t.me/joinchat/AzR3g0pKUcXt-dIUT5rMgQ" target="_blank" class="btn" id="uznat">Learn more</a>
+						</div>
+						<div class="stolbik stolbik_2">
+							<div class="h1 title">Purchase</div>
+							<div class="h4 desc">
+								Before you buy the bot, you have the opportunity to watch it in the general chat where the screenshots drop off and there is a common ...
+							</div>
+							<a href="https://t.me/joinchat/AzR3g0pKUcXt-dIUT5rMgQ" target="_blank" class="btn" id="uznat">Learn more</a>
+						</div>
+						<div class="stolbik stolbik_3">
+							<div class="h1 title">Terminals</div>
+							<div class="h4 desc">
+								Forex Glossary - novice trader's cheat sheet in the form of a collection of basic concepts and definitions, everywhere ...
+							</div>
+							<a href="https://t.me/joinchat/AzR3g0pKUcXt-dIUT5rMgQ" target="_blank" class="btn" id="uznat">Learn more</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="torg" class="torg">
+			<div class="numbers">
+				<ul>
+					<li>01</li>
+					<li>02</li>
+					<li>03</li>
+					<li>04</li>
+					<li>05</li>
+					<li class="active">06</li>
+					<li>07</li>
+					<li>08</li>
+				</ul>
+			</div>
+			<div class="container-fluid">
+				<div class="container">
+					<div class="h1 title">why it is worth starting trading with the JEK_BOT</div>
+					<div class="torg-wrap">
+<div class="row row_top">
+							<div class="wow wow_1">
+								<div class="wow-num">1</div>
+								<div class="wow-desc">
+									Thoroughly thought out system of automatic trading.
+								</div>
+							</div>
+							<div class="wow wow_2">
+								<div class="wow-num">2</div>
+								<div class="wow-desc">
+									Low risk.
+								</div>
+							</div>
+							<div class="wow wow_3">
+								<div class="wow-num">3</div>
+								<div class="wow-desc">
+									Time saving.
+								</div>
+							</div>
+							<div class="wow wow_4">
+								<div class="wow-num">4</div>
+								<div class="wow-desc">
+									However, compared to other advisers, JEK_bot works surprisingly well.
+								</div>
+							</div>
+							<div class="wow wow_5">
+								<div class="wow-num">5</div>
+								<div class="wow-desc">
+									24/7 technical support.
+								</div>				
+							</div>
+						</div>
+						<div class="row row_bottom">
+							<div class="wow wow_6">
+								<div class="wow-num">6</div>
+								<div class="wow-desc">
+									This is a working tool created by professional traders and qualified programmers, based on many years of experience and constant skill.
+								</div>								
+							</div>
+							<div class="wow wow_7">
+								<div class="wow-num">7</div>
+								<div class="wow-desc">
+									Free system updates.
+								</div>
+							</div>
+							<div class="wow wow_8">
+								<div class="wow-num">8</div>
+								<div class="wow-desc">
+									Chat bot on Telegram.
+								</div>
+							</div>
+							<div class="wow wow_9">
+								<div class="wow-num">9</div>
+								<div class="wow-desc">
+									Of course, every system, even the most sophisticated one, cannot give one hundred percent guarantee that it will work without failures and errors.
+								</div>				
+							</div>
+							<div class="wow wow_10">
+								<div class="wow-num">10</div>
+								<div class="wow-desc">
+									If for some reason independent trading is not available to you, then at first, it makes sense to start trading using an automated program
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="torg-schet-wrap">
+						<div class="h4 desc">
+							To increase its productivity, developers introduce the most effective control and analysis technologies into it.
+						</div>
+						<a class="btn btn_outline">Open an account</a>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="rob" class="rob">
+			<div class="numbers">
+				<ul>
+					<li>01</li>
+					<li>02</li>
+					<li>03</li>
+					<li>04</li>
+					<li>05</li>
+					<li>06</li>
+					<li class="active">07</li>
+					<li>08</li>
+				</ul>
+			</div>
+			<div class="container-fluid">
+				<div class="container">
+					<div class="h1 title">Start earning</div>
+					<div class="bar-wrap">
+						<div class="rob rob_top">
+							<div class="mob mob_telefon">
+								<div class="img-wrap">
+									<img src="images/telepfon.png" alt="телефон">
+								</div>
+								<div class="desc">Purchase of ATC JEK_BOT</div>
+							</div>
+							<div class="mob mob_palec">
+								<div class="img-wrap">
+									<img src="images/palec.png" alt="palec">
+								</div>
+								<div class="desc">Account Verification</div>
+							</div>
+							<div class="mob mob_bumaga">
+								<div class="img-wrap">
+									<img src="images/bumaga.png" alt="bumaga">
+								</div>
+								<div class="desc">Registration and account opening</div>
+							</div>
+							<div class="mob mob_dengi">
+								<div class="img-wrap">
+									<img src="images/dengi.png" alt="dengi">
+								</div>
+								<div class="desc">Replenishment of a trading account</div>
+							</div>
+						</div>
+						<div class="rob rob_bottom">
+							<div class="mob mob_ruki">
+								<div class="img-wrap">
+									<img src="images/ruki.png" alt="ruki">
+								</div>
+								<div class="desc">Create affiliate account</div>
+							</div>
+							<div class="mob mob_chelovek">
+								<div class="img-wrap">
+									<img src="images/chelovek.png" alt="chelovek">
+								</div>
+								<div class="desc">Installation and setup ROBOT</div>
+							</div>
+							<div class="mob mob_shest">
+								<div class="img-wrap">
+									<img src="images/shest.png" alt="shest">
+								</div>
+								<div class="desc">Install MetaTrader Terminal4</div>
+							</div>
+							<div class="mob mob_monitor">
+								<div class="img-wrap">
+									<img src="images/monitor.png" alt="monitor">
+								</div>
+								<div class="desc">Registering virtual server</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<section id="kontakni" class="kontakni">
+			<div class="numbers">
+				<ul>
+					<li>01</li>
+					<li>02</li>
+					<li>03</li>
+					<li>04</li>
+					<li>05</li>
+					<li>06</li>
+					<li>07</li>
+					<li class="active">08</li>
+				</ul>
+			</div>
+			<div class="container-fluid">
+				<div class="container">
+					<div class="h1 title">Contacts</div>
+					<div class="kontaks-wrap">
+						<div class="contact">
+							<div class="messenger">
+								<div class="mess mess_whatsapp">
+									<div class="img-wrap">
+										<img src="images/icon_10.png" alt="whatsapp"></div>
+										<div class="desc">
+											+7 (965) 569-59-88
+										</div>
+									</div>
+									<div class="mess mess_viber">
+										<div class="img-wrap">
+											<img src="images/icon_11.png" alt="viber">
+										</div>
+										<div class="desc">
+											+7 (965) 569-59-88
+										</div>
+									</div>
+									<div class="mess mess_telegram">
+										<div class="img-wrap">
+											<img src="images/icon_12.png" alt="telegram">
+										</div>
+										<div class="desc">
+											+7 (965) 569-59-88
+										</div>
+									</div>
+								</div>
+							</div>
+							<div class="contact-columns">
+								<div class="col col-tel">
+									<div class="h4 desc">
+										Do you have any questions? Feel free to ask!
+									</div>
+									<div class="sob">
+										Fill out the form and our manager will call you back soon!
+									</div>
+									<form id="contact-us" class="contact-us-form" action="">
+										<div class="contact-us-form-wrap">
+											<div class="row row-main">
+												<input class="row-input" name="name" type="text" placeholder="Your name:" required>
+												<input class="row-input" name="email" type="email" placeholder="Your e-mail:" required>
+												<input class="row-input" name="tel" type="tel" placeholder="Your phone:" required>
+											</div>
+											<div class="row row-submit">
+												<input type="submit" value="Call me">
+												<a href="https://t.me/joinchat/AzR3g0pKUcXt-dIUT5rMgQ" target="_blank" class="go-chat">Go to investor chat</a>
+											</div>									
+										</div>
+									</form>
+								</div>							
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+			<footer>
+				<div class="title">© 2020 JEK_Bot – All rights reserved</div>
+			</footer>
+			<script
+			  src="https://code.jquery.com/jquery-3.4.1.min.js"
+			  integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo="
+			  crossorigin="anonymous"></script>
+
+			<script src="js/owl/owl.carousel.min.js"></script>
+			<script src="js/fancybox/jquery.fancybox.min.js"></script>
+
+			<script>
+				(function($) {
+					
+					$('.mobile-menu-wrap').click(function() {
+						$('.menu-mob').slideToggle();
+					});
+
+					$('#contact-us').submit(function(e) {
+						e.preventDefault();
+						let inputs = $(this).serialize();
+
+						$.ajax({
+							url: "mail/mailer.php",
+							type: 'POST',
+							data: inputs,
+							success: function(res) {
+								if (res == '1') {
+									alert('Thanks for contacting us! We will call you very soon!');
+								} else {
+									alert('We apologize, an error occurred. Please contact us by messengers or call :(');
+								}
+							}
+						});
+					});
+					
+					$('.owl-carousel').owlCarousel({
+						loop:true,
+						margin:10,
+						nav:true,
+						dots:false,
+						responsive:{
+							0:{
+								items:1
+							},
+							600:{
+								items:3
+							}
+						}
+					});
+
+				})(jQuery)
+			</script>
+		</body>
+		</html>
